0d1e90a4c6fdd63b90ba64fd0c65b8c9ef260c2e
Chetyrkin Alex <pyroponya@gmail.com>
2022-11-09 15:11:09 +0300

    FLEX

--- src/components/SliderComponent.vue
+++ src/components/SliderComponent.vue
@@ -10,6 +10,7 @@
     :pagination="false"
     :navigation="false"
     :modules="modules"
+    :thumbs="{ swiper: thumbsSwiper }"
     @swiper="onSwiper"
     @slideChange="onSlideChange"
     class="mySwiper"
@@ -47,6 +48,25 @@
       </div>
     </swiper-slide>
   </swiper>
+  <swiper
+    :modules="modules"
+    :slidesPerView="3"
+    watch-slides-progress
+    @swiper="setThumbsSwiper"
+    class="thumbs_swiper"
+  >
+    <swiper-slide
+      ><div class="thumb_swiper_slide thumbs_swiper_one">ONE</div></swiper-slide
+    >
+    <swiper-slide
+      ><div class="thumb_swiper_slide thumbs_swiper_two">TWO</div></swiper-slide
+    >
+    <swiper-slide
+      ><div class="thumb_swiper_slide thumbs_swiper_three">
+        THREE
+      </div></swiper-slide
+    >
+  </swiper>
 </template>
 <script>
 // Import Swiper Vue.js components
@@ -58,7 +78,8 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 // import required modules
-import { Autoplay, Pagination, Navigation } from 'swiper';
+import { Autoplay, Pagination, Navigation, Thumbs } from 'swiper';
+import { ref } from 'vue';
 
 export default {
   components: {
@@ -66,6 +87,10 @@ export default {
     SwiperSlide,
   },
   setup(props, { emit }) {
+    const thumbsSwiper = ref(null);
+    const setThumbsSwiper = (swiper) => {
+      thumbsSwiper.value = swiper;
+    };
     const onSwiper = (swiper) => {
       console.log('onSwiper', swiper);
     };
@@ -74,9 +99,11 @@ export default {
       emit('slideChange', slideChange.activeIndex);
     };
     return {
-      modules: [Autoplay, Pagination, Navigation],
+      modules: [Autoplay, Pagination, Navigation, Thumbs],
       onSwiper,
       onSlideChange,
+      thumbsSwiper,
+      setThumbsSwiper,
     };
   },
 };
@@ -85,6 +112,40 @@ export default {
 .swiper {
   width: 100%;
   height: 100%;
+  height: 100vh;
+}
+
+.thumbs_swiper {
+  /* display: none; */
+  position: absolute;
+  height: 200px;
+  width: 100%;
+  bottom: 40px;
+}
+
+.thumb_swiper_slide {
+  width: 450px;
+  height: 180px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-color: black;
+  cursor: pointer;
+}
+
+.thumbs_swiper_one {
+  background-image: url('/src/assets/slider/slide_2.jpg');
+}
+
+.thumbs_swiper_two {
+  background-image: url('/src/assets/slider/slide_3.jpg');
+}
+
+.thumbs_swiper_three {
+  background-image: url('/src/assets/slider/slide_1.jpg');
 }
 
 .swiper-slide {
@@ -107,14 +168,14 @@ export default {
   align-items: center;
 }
 
-.swiper-slide img {
+/* .swiper-slide img {
   display: block;
   width: 100%;
   height: 100%;
   object-fit: cover;
-}
+} */
 
-.swiper {
+.mySwiper {
   margin-left: auto;
   margin-right: auto;
 }
@@ -123,8 +184,10 @@ export default {
   position: relative;
   width: 100%;
   height: 100%;
-  background-position: contain;
+  background-position: center;
+  background-size: cover;
   background-repeat: no-repeat;
+  background-color: black;
 }
 
 .slider__one {
